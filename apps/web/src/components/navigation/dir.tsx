76fd6f59888e44079f6c82397e32641eb541844b
tariik12 <tariikul12@gmail.com>
2026-07-01 23:55:12 +0600

    dir added

---
+++ apps/web/src/components/navigation/dir.tsx
@@ -0,0 +1,7 @@
+const Dir = () =>{
+    return(<div className="w-1 h-3 bg-red-500 absolute left-100">
+                
+            </div>)
+}
+
+export default Dir;
--- apps/web/src/components/navigation/secondMenu.tsx
+++ apps/web/src/components/navigation/secondMenu.tsx
@@ -16,8 +16,9 @@ useMotionValueEvent (scrollY, "change", (current) =>{
     }
 })
     return(
-         <div className={`${hidden?"border w-[70vh] mx-auto py-2 rounded-full shadow-xl  -mt-14 px-5":"border w-[140vh]  mx-auto rounded-full shadow-xl  mt-5 "}`}>
-
+         <div className={`${hidden?"border w-[70vh] mx-auto py-2 rounded-full shadow-xl  -mt-14 px-5":"border w-[100vh]  mx-auto rounded-full shadow-xl  mt-5 "}`}>
+            <div className="w-[1px] h-9 bg-[#DDDDDD] absolute left-[80.2vh] top-[13vh]"></div>
+            <div className="w-[1px] h-9 bg-[#DDDDDD] absolute right-[80.2vh] top-[13vh]"></div>
         <div className={`grid grid-cols-3 `}>
         {
             secondMenu.map((menu, index) =>(
@@ -27,7 +28,7 @@ useMotionValueEvent (scrollY, "change", (current) =>{
                <div className="  ">
                     <div >
                         {hidden?
-                        <div className={`flex  items-center justify-center ${index>0?"border-l-3 ps-[24px]":""}`}>
+                        <div className={`flex  items-center justify-between ${index>0?"border-l-3 ps-[24px]":""}  `}>
                             <div>
                            {menu.media &&  <img className="w-10" src={menu?.media} alt="home" /> }
                             <p>{menu.title}</p>
@@ -38,7 +39,7 @@ useMotionValueEvent (scrollY, "change", (current) =>{
         </div>}
                             </div>
                             :
-                            <div className={` ${index>1 && "flex justify-between items-center"} hover:bg-[#DDDDDD] py-[15px] hover:rounded-full `}>
+                            <div className={` ${index>1 && "flex justify-between items-center"}  hover:bg-[#DDDDDD] py-[15px] hover:rounded-full `}>
                                 <div className={`${index>0?" ps-[24px]":"px-5"}`}>
                                 <p className="font-[12px] w-full font-airbnb font-medium">{menu.title}</p> 
                                 <p className="font-[14px] w-full font-airbnb font-normal">{menu.description}</p>
